f0e64d5c88484bd8a40880da0730b1ebc7992668
Rapha <raraphaelar@gmail.com>
2020-11-16 15:20:08 -0300

    starting

---
+++ October2020/Assets/Scripts/ActorScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorScript : MonoBehaviour
+{
+    //Needs sprites = to amount of public enum CharacterEmotion amount
+    public Sprite[] emotionSprites;
+    SpriteRenderer SpriteRenderer;
+    public enum CharacterEmotion{
+        happy,sad,neutral,angry
+    }
+    CharacterEmotion myState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myState = CharacterEmotion.neutral;
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void ChangeState(string emotionName){
+        startCoroutine(emotionName +"State");
+
+    }
+    IEnumerator NeutralState()
+    {
+        SpriteRenderer = emotionSprites[0];
+        yield return null;
+
+    }
+      IEnumerator HappyState()
+    {
+        SpriteRenderer = emotionSprites[1];
+        yield return null;
+
+    }
+      IEnumerator SadState()
+    {
+        SpriteRenderer = emotionSprites[2];
+        yield return null;
+
+    }
+      IEnumerator AngryState()
+    {
+        SpriteRenderer = emotionSprites[3];
+        yield return null;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ October2020/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject[] characters;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i< characters.Length; i++)
+        {
+            GameObject NewActor = Instantiate(characters[i]);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeState()
+    {
+
+    }
+
+}
