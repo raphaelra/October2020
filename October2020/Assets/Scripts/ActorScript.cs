ede6bc57a98309224b9c51e180a536c1bb43eef1
Rapha <raraphaelar@gmail.com>
2020-11-19 16:40:43 -0300

    actorscript changes

--- October2020/Assets/Scripts/ActorScript.cs
+++ October2020/Assets/Scripts/ActorScript.cs
@@ -10,13 +10,43 @@ public class ActorScript : MonoBehaviour
     public enum CharacterEmotion{
         happy,sad,neutral,angry
     }
+    [SerializeField]
     CharacterEmotion myState;
     // Start is called before the first frame update
     void Start()
     {
-       
+        myState = CharacterEmotion.neutral;
+        SpriteRenderer =GetComponent<SpriteRenderer>();
+    }
+    public void ChangeState(string emotionName)
+    {
+        StartCoroutine(emotionName + "State");
     }
 
+    IEnumerator NeutralState()
+    {
+        SpriteRenderer.sprite = emotionSprites[0];
+        yield return null;
+
+    }
+        IEnumerator HappyState()
+    {
+        SpriteRenderer.sprite = emotionSprites[1];
+        yield return null;
+
+    }
+        IEnumerator SadState()
+    {
+        SpriteRenderer.sprite = emotionSprites[2];
+        yield return null;
+
+    }
+        IEnumerator AngryState()
+    {
+        SpriteRenderer.sprite = emotionSprites[3];
+        yield return null;
+
+    }
     // Update is called once per frame
     void Update()
     {
