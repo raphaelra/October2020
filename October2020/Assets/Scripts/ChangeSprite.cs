83b66be9a4bf9965b177bd813ea901ff80cde9e7
Rapha <raraphaelar@gmail.com>
2020-12-02 17:58:45 -0300

    implementing changespritetest

---
+++ October2020/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeImage : MonoBehaviour
+{
+    public GameObject RightPanel;
+    public Texture RawImage;
+    private SpriteRenderer spriteRenderer;
+     public void Start()
+     {
+         spriteRenderer = GetComponent<SpriteRenderer>();
+         if (spriteRenderer.sprite == null)
+         spriteRenderer.sprite = Resources.Load<Sprite>("rightimage"+Random.Range(0,1));
+     }
+
+
+     public void Update()
+     {
+
+     }
+     
+    // /RandomImage()
+    // {
+    //    RightPanel =GetComponent<Sprite>("rightimage"+Random.Range(0,1)).texture = texture;
+     //   spriteRenderer.sprite = Resources.Load<RawImage>("rightimage"+Random.Range(0,1));
+    //}
+ }
